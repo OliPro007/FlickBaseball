d472021f04081f253a38acbee97f767b73dde365
OliPro007 <OliPro007@gmail.com>
2015-06-07 19:10:49 -0400

    Modifier les descriptions des constantes en javadoc (Aussi rammener master comme branche principale)

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -4,16 +4,43 @@ package util;
  * Classe de déclarations publiques.
  * 
  * @author Olivier St-Jean
- * @version 04-05-2015
+ * @version 07-06-2015
  */
 public final class Constants {
 
-	public static final double G = 9.8; //constante de gravité
-	public static final double DT = 1.0/60.0; //rythme de rafraichissement des frames
-	public static final double COR_GRASS = 0.42328; //coefficient de restitution du gazon
-	public static final double COR_BAT = 0.70; //coefficient de restitution de la batte
-	public static final double KFC_GRASS = 0.35; //coefficient de frottement cinétique du gazon
-	public static final double C_COULOMB = 9*Math.pow(10, 9); //coefficient de coulomb
+	/**
+	 * Constante de gravité.
+	 */
+	public static final double G = 9.8;
+	
+	/**
+	 * Rythme de rafraichissement des frames.
+	 */
+	public static final double DT = 1.0/60.0;
+	
+	/**
+	 * Coefficient de restitution du gazon.
+	 */
+	public static final double COR_GRASS = 0.42328;
+	
+	/**
+	 * Coefficient de restitution de la batte.
+	 */
+	public static final double COR_BAT = 0.70;
+	
+	/**
+	 * Coefficient de frottement cinétique du gazon.
+	 */
+	public static final double KFC_GRASS = 0.35;
+	
+	/**
+	 * Coefficient de coulomb.
+	 */
+	public static final double C_COULOMB = 9*Math.pow(10, 9);
+	
+	/**
+	 * Coefficient de Faraday
+	 */
 	public static final double C_ELECTRIQUE = 8.85*Math.pow(10, -12);
 	
 }
